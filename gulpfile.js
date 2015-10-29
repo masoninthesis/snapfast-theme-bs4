29b20330293ee3f9da1e510dcababc580f99015a
Mason L <mason.lawlor@me.com>
2015-10-28 23:27:24 -0600

    idk

--- gulpfile.js
+++ gulpfile.js
@@ -89,3 +89,14 @@ gulp.task('js-min', ['js'], function () {
     }))
     .pipe(gulp.dest(Paths.DIST))
 })
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
